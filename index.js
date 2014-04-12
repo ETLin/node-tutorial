b0f5be5167c3a34360dd3d065328cab9f10a6249
tomchentw <developer@tomchentw.com>
2014-04-13 01:58:01 +0800

    feat(index.js): upload file example

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
+var http = require('http');
+var url = require('url');
 var fs = require('fs');
+var host = '127.0.0.1';
+var port = 5000;
 
-fs.readdir('.', function (err, filenames) {
-  if (err) { throw err; }
+http.createServer(function (req, res) {
+  var parsedUrl = url.parse(req.url);
+  console.log(parsedUrl);
 
-  filenames.forEach(function (filename) {
-    console.log('try to getting stats of [' + filename + ']');
-    fs.stat(filename, function (err, stats) {
-      if (err) { throw err; }
-      
-      var printName = filename;
-      if (stats.isDirectory()) { printName = filename.toUpperCase(); }
+  if (parsedUrl.pathname === '/') {
+    fs.readFile('./index.html', 'utf8', sendAsHTML);
 
-      console.log(printName + '(' + stats.size + ')');
+  } else if (parsedUrl.pathname === '/upload') {
+    var buffers = [];
+    req.on('data', function (chunk) { buffers.push(chunk); });
+    req.on('end', function () {
+      var buf = Buffer.concat(buffers);
+      console.log('length ['+buf.length+']');
+      fs.writeFile('./uploads.tmp', buf, function (err) {
+        if (err) { throw err; }
+        
+        fs.readFile('./upload.html', 'utf8', function (err, data) {
+          if (err) { throw err; }
+          data = data.replace('{{length}}', buf.length);
+          sendAsHTML(null, data);
+        });
+      });
     });
-  });
-});
+  } else {
+    console.log('Unknow pathname [' + parsedUrl.pathname + ']');
+  }
+
+  function sendAsHTML (err, data) {
+    if (err) { throw err; }
+    res.setHeader('Content-Type', 'text/html');
+    res.end(data);
+  }
+}).listen(port, host);
