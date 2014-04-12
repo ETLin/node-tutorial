bff8c45432244ed602ad532a0c184283bc6ab3d0
tomchentw <developer@tomchentw.com>
2014-04-13 02:25:16 +0800

    feat(index.js): switch to express.js

--- index.js
+++ index.js
@@ -1,39 +1,20 @@
-var http = require('http');
-var url = require('url');
 var fs = require('fs');
+var express = require('express');
 var host = '127.0.0.1';
 var port = 5000;
 
-http.createServer(function (req, res) {
-  var parsedUrl = url.parse(req.url);
-  console.log(parsedUrl);
+express()
+.use(express.bodyParser())
+.get('/', function (req, res) {
+  res.sendfile('./index.html');
+})
+.post('/upload', function (req, res) {
+  console.log(req.files);
 
-  if (parsedUrl.pathname === '/') {
-    fs.readFile('./index.html', 'utf8', sendAsHTML);
-
-  } else if (parsedUrl.pathname === '/upload') {
-    var buffers = [];
-    req.on('data', function (chunk) { buffers.push(chunk); });
-    req.on('end', function () {
-      var buf = Buffer.concat(buffers);
-      console.log('length ['+buf.length+']');
-      fs.writeFile('./uploads.tmp', buf, function (err) {
-        if (err) { throw err; }
-        
-        fs.readFile('./upload.html', 'utf8', function (err, data) {
-          if (err) { throw err; }
-          data = data.replace('{{length}}', buf.length);
-          sendAsHTML(null, data);
-        });
-      });
-    });
-  } else {
-    console.log('Unknow pathname [' + parsedUrl.pathname + ']');
-  }
-
-  function sendAsHTML (err, data) {
+  fs.readFile('./upload.html', 'utf8', function (err, data) {
     if (err) { throw err; }
-    res.setHeader('Content-Type', 'text/html');
-    res.end(data);
-  }
-}).listen(port, host);
+    data = data.replace('{{length}}', req.files.upload.size);
+    res.send(data);
+  });
+})
+.listen(port, host);
