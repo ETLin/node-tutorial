a806d35fe8182edb9968fe6a632c76b91c13a8b4
tomchentw <developer@tomchentw.com>
2014-04-13 13:00:09 +0800

    feat(index.js): extract models code

--- controllers/index.js
+++ controllers/index.js
@@ -1,26 +1,12 @@
 var fs = require('fs');
+var User = require('../models/user');
 
 module.exports.index = function (req, res) {
   res.render('index', {title: 'User Registration'});
 }
 
 module.exports.createUser = function (req, res) {
-  var user = req.body;
-  console.log(user);
-
-
-  fs.readFile('./users.json', 'utf8', function (err, data) {
-    var users = []
-    if (!err) {
-      users = JSON.parse(data);
-    }
-    
-    user.id = users.length;
-    users.push(user);
-    fs.writeFile('./users.json', JSON.stringify(users), function (err) {
-      if (err) { throw err; }
-
-      res.json(user);
-    });
+  User.create(req.body, function (user) {
+    res.json(user);
   });
 }
